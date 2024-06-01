9b5b0d0679129e3f6cff7d189837b75761f9a173
CarloDegasperi <carlodegasperi04@gmail.com>
2024-06-01 14:47:18 +0200

    fine programma lettura

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #define MAX_LEN 1024
 #define SPATIAL_DIM 3
 
+// creazione della struct physical system, contenente le variabili di interesse del sistema e le posizioni e velocità dei corpi 
+// ad un dato istante
 struct physical_system{
     int N;
     double G;
@@ -26,6 +28,7 @@ int main(int argc, char const *argv[])
     int ans;
     struct physical_system system;
     
+    // errore in caso non sia stato letto alcun file in input
     if (argc < 2)
     {
         fprintf(stderr, "Non è stato specificato un file di input.\n");
@@ -33,34 +36,44 @@ int main(int argc, char const *argv[])
     }
 
     inFile = fopen(argv[1], "r");
-
+    // errore in caso ci siano stati problemi nell'apertura del file
     if (inFile == NULL)
     {
         fprintf(stderr, "Impossibile aprire il file: %s\n", argv[1]);
         return 1;
     }
 
+    // ciclo per la lettura dei dati di input dal file fornito in esecuzione
     while ( (ans = read_input(inFile, &system)) != -1 )
     {   
-        int n=0;
-        printf("%d", n);
         if ( ans == -2 )
         {
             fprintf(stderr, "Errore nella lettura del file in input: il file deve contenere N, G, dt, tdump e T");
         }
     }
-    /*
-    for (int i=0; i<(SPATIAL_DIM*system.N); i++)
+    
+    // -DA TOGLIERE- serve solo per stampare i dati di input in modo da capire se tutto funzioni
+    for (int i=0; i<SPATIAL_DIM*system.N; i++)
     {
         printf("%lf\t", system.vel[i]);
     }
-    */
-
+    
     fclose(inFile);
+
     free(system.pos);
     free(system.vel);
+
+    return 0;
 }
 
+/**
+ * Funzione che legge i dati di input a partire dal file fornito in esecuzione (ogni volta che viene chiamata legge una riga)
+ * 
+ * @param inFile puntatore al file fornito in esecuzione
+ * @param system puntatore alla struct contenente i dati relativi al sistema fisico considerato
+ * 
+ * @return -1 per End of File; -2 in caso di errore; 0 di default
+ */
 int read_input(FILE *inFile, struct physical_system *system)
 {
     char line[MAX_LEN], str[5];
@@ -70,6 +83,8 @@ int read_input(FILE *inFile, struct physical_system *system)
     {
         return -1;
     }
+
+    // serie di controlli che cerca il N, G, dt, tdump e T nell'header e che esclude eventuali commenti 
     if (line[0] == '#')
     {
         sscanf(line, "%4s", str);
@@ -104,35 +119,42 @@ int read_input(FILE *inFile, struct physical_system *system)
         }
         return 0;
     }
+
+    // controllo che siano stati letti i dati necessari per l'esecuzione del programma
     if ( !system->N || !system->G || !system->dt || !system->tdump || !system->T )
     {
         return -2;
     }
 
-    int nChar, nTotChar, nBody;
-    if ((system->pos = (double *)malloc(system->N*SPATIAL_DIM*sizeof(double))) == NULL) {
-        fprintf(stderr, "errore nella collocazione della memoria");
-        exit(EXIT_FAILURE);
-    }
-    if ((system->vel = (double *)malloc(system->N*SPATIAL_DIM*sizeof(double))) == NULL) {
-        fprintf(stderr, "errore nella collocazione della memoria");
-        exit(EXIT_FAILURE);
-    }
+    int nChar, nTotChar, bodyNumber;
 
-    sscanf(line, "%d %n", &nBody, &nTotChar);
-    printf("%d", nBody);
+    // codice che permette di puntare in modo "static" alla heap, in questo modo creiamo due
+    // vettori di dimensione dinamica direttamente nella funzione (che vengono inizializzati solo una volta per esecuzione)
+    static double *pos = NULL;
+    if(!pos) pos = (double *)malloc( system->N * SPATIAL_DIM * sizeof(double) ); 
+    static double *vel = NULL;
+    if(!vel) vel = (double *)malloc( system->N * SPATIAL_DIM * sizeof(double) );
+
+    // assegnazione dei puntatori appena inizializzati ai puntatori della struct
+    system->pos = pos;
+    system->vel = vel;
+    
+    // lettura del numero di corpo di cui si stanno leggendo posizioni e velocità di partenza
+    sscanf(line, "%d %n", &bodyNumber, &nTotChar);
    
+    // ciclo per la lettura della posizione di partenza del corpo specificato da bodyNumber
     for (int i=0; i<SPATIAL_DIM; i++)
     {   
-        sscanf(line + nTotChar, "%lf %n", system->pos + i + SPATIAL_DIM * (nBody-1), &nChar);
-        printf("%lf", system->pos[i+SPATIAL_DIM*(nBody-1)]); 
+        sscanf(line + nTotChar, "%lf %n", system->pos + i + SPATIAL_DIM * (bodyNumber-1), &nChar);
         nTotChar += nChar;
     }
 
+    // ciclo per la lettura della velocità di partenza del corpo specificato da bodyNumber
     for (int i=0; i<SPATIAL_DIM; i++)
     {
-        sscanf(line + nTotChar, "%lf %n", system->vel + i + SPATIAL_DIM * (nBody-1), &nChar);
+        sscanf(line + nTotChar, "%lf %n", vel + i + SPATIAL_DIM * (bodyNumber-1), &nChar);
         nTotChar += nChar;
     }
+    
     return 0;
 }
