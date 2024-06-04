65d16f7f458d05c7cf4778e29a614c9e2a549e03
lorenzob0nati <lorenzo.bonati@studenti.unitn.it>
2024-06-04 08:26:26 +0200

    Update geom.c

--- geom.c
+++ geom.c
@@ -5,12 +5,7 @@
  * v2 = ( 1.000000 1.000000)
  * la norma del vettore distanza: 1.414214
  */
-/**prova di *vec_sum
- * v1 = ( 1.000000 4.000000 1.000000)
- * v2 = ( 2.000000 2.000000 2.000000)
- * Il vettore somma risulta: 
- * v_tot = ( 3.000000 6.000000 3.000000)
- */
+
 /**prova Ekin 
  * vel = ( 0.000000 1.000000 1.000000)
  * mass = ( 1.000000 2.000000 3.000000)
@@ -20,40 +15,18 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
-#include <stdlib.h>
-
 #define N_OPS 6
 #define MAX_LEN 100
 #define SPATIAL_DIM 2
-double *vec_sum(const double *vec1, const double *vec2, const int dim);
-double vec_dist(const double *vec1, const double *vec2, const int dim);
+
+
 double dist(const double *vec1, const double *vec2, const int dim);
 double scal(const double *vec1, const double *vec2, const int dim);
 double Ekin(const double *velVec, const double *mass, const int nBodies);
 double Epot(const double *posVec, const double *mass, const double G, const int nBodies);
+void vec_dist(const double *vec1, const double *vec2, double *vec_d, const int dim);
 
 
-
-
-/**
- * Funzione che calcola il vettore somma tra 2 vettori di dimensione specificata
- *
- * @param vec1 Puntatore al primo vettore di double
- * @param vec2 Puntatore al secondo vettore di double
- * @param dim Numero intero della dimensione dei vettori
- *
- * @return Vettore double risultato della somma vettoriale
- */
-double *vec_sum(const double *vec1, const double *vec2, int dim)
-{
-    double *v_tot = malloc(dim * sizeof(double));
-    for (int i = 0; i < dim; i++)
-    {
-        v_tot[i] = vec1[i] + vec2[i];
-    }
-    return v_tot;
-}
 
 /**
  * Funzione che restituisce un vettore contenente le distanze delle posizioni tra il i-esimo corpo e il j-esimo (i,j = 1, 2, 3)
@@ -61,21 +34,19 @@ double *vec_sum(const double *vec1, const double *vec2, int dim)
  * @param vec1 Puntatore al primo vettore di double
  * @param vec2 Puntatore al secondo vettore di double
  * @param vec3 Puntatore al terzo vettore di double
+ * @param vec_d Puntatore al vettore risultato di double
  * @param dim Numero intero della dimensione dei vettori
  *
  * @return puntatore a vettore double risultato delle distanze delle posizioni dei corpi
  */
-double vec_dist(const double *vec1, const double *vec2, int dim)
+void vec_dist(const double *vec1, const double *vec2, double *vec_d, const int dim)
 {
-    double *v_dist = malloc(dim * sizeof(double));
-    double dist;
-    for (int i = 0; i < dim; i++)
-    {
-        v_dist[i] = abs(vec1[i] - vec2[i]);
-    }
-    dist = norm(v_dist, dim);
-    return dist;
+        for(int i = 0; i < dim; i++)
+        {
+                vec_d[i] = vec1[i] - vec2[i];
+        }
 }
+
 
 /**
  * Funzione che calcola la distanza tra 2 vettori di dimensione specificata
@@ -138,26 +109,36 @@ double Ekin(const double *velVec, const double *mass, const int nBodies)
     double v;
     for(int i = 0; i < nBodies + 1; i++)
     {
-        if(mass[i] = mass[i + 1])
+        if(mass[i] == mass[i + 1])
         {
-            count = count + 1
+            count = count + 1;
         }
     }
     if(count == mass[0] * nBodies)
-    { 
-        Ekin = 0.5 * scal(velVec, velVec, SPATIAL_DIM * nBodies);    
+    {
+        Ekin = 0.5 * scal(velVec, velVec, SPATIAL_DIM * nBodies);
     }
     else
     {
         for(int i = 0; i < nBodies; i++)
-        { 
+        {
             v = velVec[i];
-            Ekin = Ekin + 0.5 * mass[i] * (v * v); 
+            Ekin = Ekin + 0.5 * mass[i] * (v * v);
         }
     }
     return Ekin;
-}	
+}
 
+/**
+ * Funzione che calcola l'energia potenziale del sistema a 3 corpi
+ *
+ * @param posVec Puntatore al primo vettore di doublle (posizioni) 
+ * @param mass Puntatore al secondo vettore di double (masse)
+ * @param G costante di gravitazione universale
+ * @param nBodies Numero intero della dimensione dei vettori
+ *
+ * @return Valore double dell'energia potenziale
+ */
 double Epot(const double *posVec, const double *mass, const double G, const int nBodies)
 {
     double potEnergyTot = 0;
@@ -165,12 +146,14 @@ double Epot(const double *posVec, const double *mass, const double G, const int 
     {
         for (int j = i + 1; j < nBodies; j++)
         {
-            double coords1[SPATIAL_DIM] = {posVec[2 * i], posVec[2 * i + 1]};
-            double coords2[SPATIAL_DIM] = {posVec[2 * j], posVec[2 * j + 1]};
-            double distance = dist(coords1, coords2, 2);
-            potEnergyTot += -G / distance;
+            double coords1[SPATIAL_DIM] = {posVec[3 * i], posVec[3 * i + 1], posVec[3 * i + 2]};
+            double coords2[SPATIAL_DIM] = {posVec[3 * j], posVec[3 * j + 1], posVec[3 * j + 2]};
+            double distance = dist(coords1, coords2, 3);
+            potEnergyTot += -G*(mass[i] * mass[i + 1])/ distance;
         }
     }
 
     return potEnergyTot;
 }
+
+
