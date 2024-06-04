fa316dd7fcf47978bc21c4742f6f6ee78b554adc
CarloDegasperi <carlodegasperi04@gmail.com>
2024-06-04 19:34:26 +0200

    prime modifiche

--- geom.c
+++ geom.c
@@ -1,4 +1,4 @@
-// gcc GeomCalc.c -std=c99 -o GeomCalc.exe -lm -Wall -Wpedantic
+// gcc geom.c -std=c99 -o geom.exe -lm -Wall -Wpedantic
 
 /**prova di vec_dist
  * v1 = ( 0.000000 0.000000)
@@ -8,21 +8,22 @@
 
 /**prova Ekin
  * vel = ( 0.000000 1.000000 1.000000)
- * mass = ( 1.000000 2.000000 3.000000)
+ * masses = ( 1.000000 2.000000 3.000000)
  * energia cinetica risulta: 2.500000
  */
 
 #include <stdio.h>
 #include <math.h>
-#define N_OPS 6
+#define N_OPS 5
+// a cosa serve questo? Non ho capito bene
 #define MAX_LEN 100
-#define SPATIAL_DIM 2
+#define SPATIAL_DIM 3
 
 double dist(const double *vec1, const double *vec2, const int dim);
 double scal(const double *vec1, const double *vec2, const int dim);
-double Ekin(const double *velVec, const double *mass, const int nBodies);
-double Epot(const double *posVec, const double *mass, const double G, const int nBodies);
 void vec_dist(const double *vec1, const double *vec2, double *vec_d, const int dim);
+double Ekin(const double *velVec, const double *masses, const int nBodies);
+double Epot(const double *posVec, const double *masses, const double G, const int nBodies);
 
 /**
  * Funzione che restituisce un vettore contenente le distanze delle posizioni tra il i-esimo corpo e il j-esimo (i,j = 1, 2, 3)
@@ -31,8 +32,6 @@ void vec_dist(const double *vec1, const double *vec2, double *vec_d, const int d
  * @param vec2 Puntatore al secondo vettore di double
  * @param vec_d Puntatore al vettore risultato di double
  * @param dim Numero intero della dimensione dei vettori
- *
- * @return puntatore a vettore double risultato delle distanze delle posizioni dei corpi
  */
 void vec_dist(const double *vec1, const double *vec2, double *vec_d, const int dim)
 {
@@ -90,25 +89,25 @@ double scal(const double *vec1, const double *vec2, const int dim)
  * Funzione che calcola l'energia cinetica del sistema a 3 corpi
  *
  * @param velVec Puntatore al primo vettore di doublle (velocità)
- * @param mass Puntatore al secondo vettore di double (masse)
+ * @param masses Puntatore al secondo vettore di double (masse)
  * @param nBodies Numero intero della dimensione dei vettori
  *
  * @return Valore double dell'energia cinetica
  */
 
-double Ekin(const double *velVec, const double *mass, const int nBodies)
+double Ekin(const double *velVec, const double *masses, const int nBodies)
 {
     int count = 0;
     double Ekin = 0;
     double v;
     for (int i = 0; i < nBodies + 1; i++)
     {
-        if (mass[i] == mass[i + 1])
+        if (masses[i] == masses[i + 1])
         {
             count = count + 1;
         }
     }
-    if (count == mass[0] * nBodies)
+    if (count == masses[0] * nBodies)
     {
         Ekin = 0.5 * scal(velVec, velVec, SPATIAL_DIM * nBodies);
     }
@@ -117,7 +116,7 @@ double Ekin(const double *velVec, const double *mass, const int nBodies)
         for (int i = 0; i < nBodies; i++)
         {
             v = velVec[i];
-            Ekin = Ekin + 0.5 * mass[i] * (v * v);
+            Ekin = Ekin + 0.5 * masses[i] * (v * v);
         }
     }
     return Ekin;
@@ -127,13 +126,13 @@ double Ekin(const double *velVec, const double *mass, const int nBodies)
  * Funzione che calcola l'energia potenziale del sistema a 3 corpi
  *
  * @param posVec Puntatore al primo vettore di doublle (posizioni)
- * @param mass Puntatore al secondo vettore di double (masse)
+ * @param masses Puntatore al secondo vettore di double (masse)
  * @param G costante di gravitazione universale
  * @param nBodies Numero intero della dimensione dei vettori
  *
  * @return Valore double dell'energia potenziale
  */
-double Epot(const double *posVec, const double *mass, const double G, const int nBodies)
+double Epot(const double *posVec, const double *masses, const double G, const int nBodies)
 {
     double potEnergyTot = 0;
     for (int i = 0; i < nBodies; i++)
@@ -143,7 +142,7 @@ double Epot(const double *posVec, const double *mass, const double G, const int 
             double coords1[SPATIAL_DIM] = {posVec[3 * i], posVec[3 * i + 1], posVec[3 * i + 2]};
             double coords2[SPATIAL_DIM] = {posVec[3 * j], posVec[3 * j + 1], posVec[3 * j + 2]};
             double distance = dist(coords1, coords2, 3);
-            potEnergyTot += -G * (mass[i] * mass[i + 1]) / distance;
+            potEnergyTot += -G * (masses[i] * masses[i + 1]) / distance;
         }
     }
 
