601d7d66076041928733310d6caf96d7abc21cf6
lorenzob0nati <lorenzo.bonati@studenti.unitn.it>
2024-06-06 13:59:37 +0200

    Update geom.h

--- geom.h
+++ geom.h
@@ -1,4 +1,31 @@
-double dist(double *vec1, double *vec2, int dim);
-void vec_dist(double *vec1, double *vec2, double *vec_d, int dim);
-double Ekin(double *vel, double *masses, int nBodies);
-double Epot(double *coord, double *masses, double G, int nBodies);
+/**
+ * Funzione che riempie vec_d con il vettore differenza tra vec1 e vec2
+ *
+ * @param vec1 Puntatore al primo vettore di double
+ * @param vec2 Puntatore al secondo vettore di double
+ * @param vec_d Puntatore al vettore risultato di double
+ * @param dim Numero intero della dimensione dei vettori
+ */
+void vec_diff(const double *vec1, const double *vec2, double *vec_d, const int dim);
+
+/**
+ * Funzione che calcola la distanza tra 2 vettori di dimensione specificata
+ *
+ * @param vec1 Puntatore al primo vettore di double
+ * @param vec2 Puntatore al secondo vettore di double
+ * @param dim Numero intero della dimensione dei vettori
+ *
+ * @return Valore double della distanza euclidea tra i due vettori
+ */
+double dist(const double *vec1, const double *vec2, const int dim);
+
+/**
+ * Funzione che calcola il prodotto scalare tra 2 vettori di dimensione specificata
+ *
+ * @param vec1 Puntatore al primo vettore di double
+ * @param vec2 Puntatore al secondo vettore di double
+ * @param dim Numero intero della dimensione dei vettori
+ *
+ * @return Valore double risultato dal prodotto scalare
+ */
+double scal(const double *vec1, const double *vec2, const int dim);
