18e48ab553e9888a371e5292dbbd1cf50eb767e7
McGyv246 <122812734+McGyv246@users.noreply.github.com>
2024-06-07 19:04:17 +0200

    Aggiunte include guards nei file .h

--- integrator.h
+++ integrator.h
@@ -1,2 +1,7 @@
+#ifndef INTEGRATOR_H
+#define INTEGRATOR_H
+
 int velverlet_ndim_npart(const long double dt, const long double forceConst, const int nBodies, const int spatialDim, const long double *masses,
                          long double *coord, long double *vel, long double *force, long double **f_o, void (*F)(const long double *, const long double *, const long double, const int, long double *));
+
+#endif
