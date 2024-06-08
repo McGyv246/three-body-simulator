e9485d079eef858f2e89ac0a608d4b4931ee610d
lorenzob0nati <lorenzo.bonati@studenti.unitn.it>
2024-06-08 10:08:35 +0200

    Geom (#35)
    * Update geom.h
    
    * Update geom.c
    
    * Update integrator.h
    
    * Update integrator.c
    
    ---------
    
    Co-authored-by: McGyv246 <122812734+McGyv246@users.noreply.github.com>

--- geom.c
+++ geom.c
@@ -13,18 +13,6 @@
 #include <stdio.h>
 #include <math.h>
 
-void vec_diff(const long double *vec1, const long double *vec2, long double *vec_d, const int dim);
-long double dist(const long double *vec1, const long double *vec2, const int dim);
-long double scal(const long double *vec1, const long double *vec2, const int dim);
-
-/**
- * Funzione che riempie vec_d con il vettore differenza tra vec1 e vec2
- *
- * @param vec1 Puntatore al primo vettore di long double
- * @param vec2 Puntatore al secondo vettore di long double
- * @param vec_d Puntatore al vettore risultato di long double
- * @param dim Numero intero della dimensione dei vettori
- */
 void vec_diff(const long double *vec1, const long double *vec2, long double *vec_d, const int dim)
 {
     for (int i = 0; i < dim; i++)
@@ -33,15 +21,6 @@ void vec_diff(const long double *vec1, const long double *vec2, long double *vec
     }
 }
 
-/**
- * Funzione che calcola la distanza tra 2 vettori di dimensione specificata
- *
- * @param vec1 Puntatore al primo vettore di long double
- * @param vec2 Puntatore al secondo vettore di long double
- * @param dim Numero intero della dimensione dei vettori
- *
- * @return Valore long double della distanza euclidea tra i due vettori
- */
 long double dist(const long double *vec1, const long double *vec2, const int dim)
 {
     long double sumSquared = 0.L;
@@ -56,15 +35,6 @@ long double dist(const long double *vec1, const long double *vec2, const int dim
     return sqrt(sumSquared);
 }
 
-/**
- * Funzione che calcola il prodotto scalare tra 2 vettori di dimensione specificata
- *
- * @param vec1 Puntatore al primo vettore di long double
- * @param vec2 Puntatore al secondo vettore di long double
- * @param dim Numero intero della dimensione dei vettori
- *
- * @return Valore long double risultato dal prodotto scalare
- */
 long double scal(const long double *vec1, const long double *vec2, const int dim)
 {
     long double sum = 0.L;
--- geom.h
+++ geom.h
@@ -1,8 +1,36 @@
 #ifndef GEOM_H
 #define GEOM_H
 
+/**
+ * Funzione che riempie vec_d con il vettore differenza tra vec1 e vec2
+ *
+ * @param vec1 Puntatore al primo vettore di long double
+ * @param vec2 Puntatore al secondo vettore di long double
+ * @param vec_d Puntatore al vettore risultato di long double
+ * @param dim Numero intero della dimensione dei vettori
+ */
 void vec_diff(const long double *vec1, const long double *vec2, long double *vec_d, const int dim);
+
+/**
+ * Funzione che calcola la distanza tra 2 vettori di dimensione specificata
+ *
+ * @param vec1 Puntatore al primo vettore di long double
+ * @param vec2 Puntatore al secondo vettore di long double
+ * @param dim Numero intero della dimensione dei vettori
+ *
+ * @return Valore long double della distanza euclidea tra i due vettori
+ */
 long double dist(const long double *vec1, const long double *vec2, const int dim);
+
+/**
+ * Funzione che calcola il prodotto scalare tra 2 vettori di dimensione specificata
+ *
+ * @param vec1 Puntatore al primo vettore di long double
+ * @param vec2 Puntatore al secondo vettore di long double
+ * @param dim Numero intero della dimensione dei vettori
+ *
+ * @return Valore long double risultato dal prodotto scalare
+ */
 long double scal(const long double *vec1, const long double *vec2, const int dim);
 
 #endif
